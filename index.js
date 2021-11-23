d8fdee7f30541b14ff8b4d1d83606d3813f1ded4
farizaiis <farizaiis@hotmail.com>
2021-11-23 14:18:12 +0700

    make function for getting random colour

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // alert("Test js")
 
+//Declare button colours
+let buttonColours = ["red", "blue", "green", "yellow"]
+
+let gamePattern = [];
+
+
+function nextSequence() {
+    //Get random number 0-3 for getting index number
+    let randomNumber = Math.floor(Math.random() * 4)
+
+    //Get element by index that randomized
+    let randomChosenColour = buttonColours[randomNumber]
+
+    //Push element that has been randomized
+    gamePattern.push(randomChosenColour)
+}

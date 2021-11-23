37fe004e9187b1afdea545e59c253580850f14f7
farizaiis <farizaiis@hotmail.com>
2021-11-23 14:30:35 +0700

    add function that will be checked which button has pressed by user

--- index.js
+++ index.js
@@ -3,8 +3,21 @@
 //Declare button colours
 let buttonColours = ["red", "blue", "green", "yellow"]
 
+//Pattern that game request
 let gamePattern = [];
 
+//Pattern that return from which user click
+var userClickedPattern = [];
+
+//Detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function() {
+    //Store the id of the button that got clicked.
+    let userChosenColour = $(this).attr("id");
+
+    //Push element that has been select/click by user variable userClickedPattern
+    userClickedPattern.push(userChosenColour);
+
+})
 
 function nextSequence() {
     //Get random number 0-3 for getting index number
